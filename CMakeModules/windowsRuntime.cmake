0a325bd75e7dbd64b02b19fba5df63c70c232bec
jcy <2821872892@qq.com>
2026-07-30 23:58:41 +0800

    refactor: simplify Windows runtime staging

--- CMakeModules/windowsRuntime.cmake
+++ CMakeModules/windowsRuntime.cmake
@@ -4,88 +4,6 @@ option(
   ON
 )
 
-function(pgo_stage_windows_runtime_for_module target)
-  if(NOT TARGET pgo_windows_runtime)
-    return()
-  endif()
-
-  add_dependencies(${target} pgo_windows_runtime)
-  add_custom_command(
-    TARGET ${target}
-    POST_BUILD
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory_if_different
-      "${PGO_WINDOWS_RUNTIME_OUTPUT_DIRECTORY}"
-      "$<TARGET_FILE_DIR:${target}>"
-    COMMENT "Staging Windows runtime DLLs beside $<TARGET_FILE_NAME:${target}>"
-    VERBATIM
-  )
-endfunction()
-
-function(_pgo_collect_project_runtime_targets directory)
-  get_property(_pgo_targets DIRECTORY "${directory}" PROPERTY BUILDSYSTEM_TARGETS)
-  foreach(_pgo_target IN LISTS _pgo_targets)
-    get_target_property(_pgo_target_imported "${_pgo_target}" IMPORTED)
-    if(_pgo_target_imported)
-      continue()
-    endif()
-
-    get_target_property(_pgo_target_source_dir "${_pgo_target}" SOURCE_DIR)
-    set(_pgo_source_root "${CMAKE_SOURCE_DIR}")
-    cmake_path(
-      IS_PREFIX
-      _pgo_source_root
-      "${_pgo_target_source_dir}"
-      NORMALIZE
-      _pgo_is_project_target
-    )
-    if(NOT _pgo_is_project_target)
-      continue()
-    endif()
-
-    get_target_property(_pgo_target_type "${_pgo_target}" TYPE)
-    if(
-      NOT _pgo_target_type STREQUAL "EXECUTABLE"
-      AND NOT _pgo_target_type STREQUAL "SHARED_LIBRARY"
-    )
-      continue()
-    endif()
-
-    set_target_properties(
-      ${_pgo_target}
-      PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY
-          "${CMAKE_BINARY_DIR}/bin/$<CONFIG>"
-        RUNTIME_OUTPUT_DIRECTORY_DEBUG
-          "${CMAKE_BINARY_DIR}/bin/Debug"
-        RUNTIME_OUTPUT_DIRECTORY_RELEASE
-          "${CMAKE_BINARY_DIR}/bin/Release"
-        RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO
-          "${CMAKE_BINARY_DIR}/bin/RelWithDebInfo"
-        RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL
-          "${CMAKE_BINARY_DIR}/bin/MinSizeRel"
-    )
-    add_dependencies(${_pgo_target} pgo_windows_runtime)
-  endforeach()
-
-  get_property(
-    _pgo_subdirectories
-    DIRECTORY "${directory}"
-    PROPERTY SUBDIRECTORIES
-  )
-  foreach(_pgo_subdirectory IN LISTS _pgo_subdirectories)
-    _pgo_collect_project_runtime_targets("${_pgo_subdirectory}")
-  endforeach()
-endfunction()
-
-function(pgo_finalize_windows_runtime_targets)
-  if(NOT TARGET pgo_windows_runtime)
-    return()
-  endif()
-
-  _pgo_collect_project_runtime_targets("${CMAKE_SOURCE_DIR}")
-endfunction()
-
 if(NOT WIN32 OR NOT PGO_STAGE_WINDOWS_RUNTIME)
   return()
 endif()
@@ -106,42 +24,41 @@ if(NOT IS_DIRECTORY "${_pgo_windows_python_bin}")
     "${_pgo_windows_python_bin}")
 endif()
 
-set(_pgo_windows_runtime_dlls
-  "${_pgo_windows_python_bin}/tbb12.dll"
-  "${_pgo_windows_python_bin}/tbbmalloc.dll"
+file(GLOB _pgo_tbb_runtime_dlls
+  CONFIGURE_DEPENDS
+  LIST_DIRECTORIES FALSE
+  "${_pgo_windows_python_bin}/tbb*.dll")
+if(NOT _pgo_tbb_runtime_dlls)
+  message(FATAL_ERROR
+    "No TBB runtime DLLs found in ${_pgo_windows_python_bin}")
+endif()
+
+set(_pgo_windows_runtime_dlls ${_pgo_tbb_runtime_dlls})
+
+if(PGO_USE_MKL)
+  file(GLOB _pgo_mkl_runtime_dlls
+    CONFIGURE_DEPENDS
+    LIST_DIRECTORIES FALSE
+    "${_pgo_windows_python_bin}/mkl_*.dll")
+  if(NOT _pgo_mkl_runtime_dlls)
+    message(FATAL_ERROR
+      "No MKL runtime DLLs found in ${_pgo_windows_python_bin}")
+  endif()
+  list(APPEND _pgo_windows_runtime_dlls ${_pgo_mkl_runtime_dlls})
+endif()
+
+set(_pgo_project_runtime_dlls
   "${CMAKE_SOURCE_DIR}/third-party/gmp-msvc/release/gmp-10.dll"
   "${CMAKE_SOURCE_DIR}/third-party/gmp-msvc/release/gmpxx-4.dll"
   "${CMAKE_SOURCE_DIR}/third-party/mpfr-msvc/release/mpfr-6.dll"
 )
-
-if(PGO_USE_MKL)
-  foreach(_pgo_mkl_runtime_name
-      core
-      intel_lp64
-      tbb_thread
-      def
-      mc3
-      avx2
-      avx512
-      vml_def
-      vml_cmpt
-      vml_mc3
-      vml_avx2
-      vml_avx512)
-    list(APPEND
-      _pgo_windows_runtime_dlls
-      "${_pgo_windows_python_bin}/mkl_${_pgo_mkl_runtime_name}.2.dll"
-    )
-  endforeach()
-endif()
-
-foreach(_pgo_windows_required_dll IN LISTS _pgo_windows_runtime_dlls)
+foreach(_pgo_windows_required_dll IN LISTS _pgo_project_runtime_dlls)
   if(NOT EXISTS "${_pgo_windows_required_dll}")
     message(FATAL_ERROR
-      "Required Windows runtime DLL is missing: "
-      "${_pgo_windows_required_dll}")
+      "Project runtime DLL is missing: ${_pgo_windows_required_dll}")
   endif()
 endforeach()
+list(APPEND _pgo_windows_runtime_dlls ${_pgo_project_runtime_dlls})
 
 list(REMOVE_DUPLICATES _pgo_windows_runtime_dlls)
 list(SORT _pgo_windows_runtime_dlls)
@@ -152,18 +69,6 @@ set(
   CACHE INTERNAL
   "Shared Windows source-build runtime directory"
   FORCE
-)
-
-set(_pgo_windows_runtime_manifest "")
-foreach(_pgo_windows_runtime_dll IN LISTS _pgo_windows_runtime_dlls)
-  string(APPEND
-    _pgo_windows_runtime_manifest
-    "${_pgo_windows_runtime_dll}\n")
-endforeach()
-file(
-  WRITE
-  "${CMAKE_BINARY_DIR}/windows-runtime-manifest.txt"
-  "${_pgo_windows_runtime_manifest}"
 )
 
 add_custom_target(
@@ -181,5 +86,7 @@ add_custom_target(
   VERBATIM
 )
 
-message(STATUS
-  "Windows runtime staging: ${PGO_WINDOWS_RUNTIME_OUTPUT_DIRECTORY}")
+list(LENGTH _pgo_windows_runtime_dlls _pgo_windows_runtime_dll_count)
+message(STATUS "Windows runtime staging: "
+  "${PGO_WINDOWS_RUNTIME_OUTPUT_DIRECTORY} "
+  "(${_pgo_windows_runtime_dll_count} DLLs)")
